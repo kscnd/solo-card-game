e10b1f886dabab9806bbc5204b301c2e2a4da9ce
kscnd <distynova.au@gmail.com>
2026-05-20 21:15:58 +0900

    카드 뽑기 방식 수정

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-/* 플레이어는 게임을 시작할 때 플레이어 이름으로 된 배열을 만들어야 함. 
-만약 플레이어 이름이 abc라면 abc = ["abc"] 와 같이 입력해야 함.
-*/
-let cardList = [];
+const numList = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const suitList = ["♠", "♦", "♥", "♣"];
+let player = [];
+let dealer = [];
+
 function reset() {
-    cardList = [];
-    for (let i = 0; i < 4; i++) cardList.push(Array(14));
-    cardList[0][0] = "♠";
-    cardList[1][0] = "♦";
-    cardList[2][0] = "♥";
-    cardList[3][0] = "♣";
+    player = [];
+    dealer = [];
 }
 
-function draw(player) {
-    let n = Math.floor(Math.random() * 3);
-    let m = Math.floor(Math.random() * 12 + 1);
-    while (cardList[n][m] != undefined) {
-        n = Math.floor(Math.random() * 3);
-        m = Math.floor(Math.random() * 12 + 1);
+function draw(object) {
+    let n = Math.floor(Math.random() * 13);
+    let m = Math.floor(Math.random() * 4);
+    while (player.indexOf(numList[n] + suitList[m]) !== -1 && dealer.indexOf(numList[n] + suitList[m]) !== -1) {
+        n = Math.floor(Math.random() * 13);
+        m = Math.floor(Math.random() * 4);
     }
-    cardList[n][m] = player[0];
-    player.push([m, cardList[n][0]]);
+    object.push(numList[n] + suitList[m]);
 }
