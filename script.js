36f58e0f8e5888d7d964e4c30160bda52fae4bed
kscnd <distynova.au@gmail.com>
2026-05-22 23:23:45 +0900

    게임모드 기초 시스템 제작, 플레이어의 blackjack과 bust 판정 추가
    추가: 게임모드 변경, 게임 재시작, 숫자 변경 기능, 플레이어의 버스트와 블랙잭 통한 게임 종료

--- script.js
+++ script.js
@@ -1,19 +1,69 @@
 const numList = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 const suitList = ["♠", "♦", "♥", "♣"];
+let cardList = [];
 let player = [];
 let dealer = [];
+let mode = "main";
+let state = "ready";
+const game = document.getElementsByClassName("game");
 
-function reset() {
+function modeChange(input) {
+    document.getElementById(mode).style.display = "none";
+    mode = input;
+    document.getElementById(mode).style.display = "block";
+    reset();
+}
+function reset() { //게임 끝난 후 모드 변경 없이 리셋 때 필요
     player = [];
     dealer = [];
+    cardList = [];
+    state = "ready";
+    document.getElementById("dealer").innerText = "카드를 뽑아 시작하세요";
+    document.getElementById("player").innerText = "카드를 뽑아 시작하세요";
 }
 
 function draw(object) {
+    if (state === "ended") return;
     let n = Math.floor(Math.random() * 13);
     let m = Math.floor(Math.random() * 4);
-    while (player.indexOf(numList[n] + suitList[m]) !== -1 && dealer.indexOf(numList[n] + suitList[m]) !== -1) {
+    while (cardList.indexOf(numList[n] + suitList[m]) !== -1) { //무한반복 방지할 것 만들어야 함
         n = Math.floor(Math.random() * 13);
         m = Math.floor(Math.random() * 4);
     }
     object.push(numList[n] + suitList[m]);
+    cardList.push(numList[n] + suitList[m]);
+    switch (mode) {
+        case "blackjack":
+            blackjack(object);
+            break;
+    }
+}
+
+function blackjack(object) {
+    document.getElementById("dealer").innerText = dealer;
+    document.getElementById("player").innerText = player;
+    let sum = 0;
+    object.forEach((n) => sum += numChange(n));
+    if (object === player) {
+        if (sum > 21) {
+            document.getElementById("dealer").innerText = "버스트! 딜러가 승리하였습니다";
+            state = "ended";
+        } else if(sum === 21) {
+            document.getElementById("dealer").innerText = "BLACKJACK!";
+            state = "ended";
+        }
+        return sum;
+    }   
+}
+function numChange(n) { //로마자 카드 숫자로 바꾸는 것. 추후에 A를 경우에 따라 1 또는 14로 바꾸는 기능 만들어야 함
+    switch (n[0]) {
+        case "A":
+            return 11;
+        case "J":
+        case "Q":
+        case "K":
+            return 10;
+        default:
+            return Number(n.replace(n[n.length - 1], ''));
+    }
 }
